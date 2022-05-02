12e87c4bd1264c244558917cb86a96ed8f5ef54c
Kimchiman <asd14235714@gmail.com>
2022-05-02 12:09:55 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/ramyun_and_jumokbob/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.ramyun_and_jumokbob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
